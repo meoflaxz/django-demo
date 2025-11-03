c366fb3a9c6c97c70f7deb77cababe8c83d53ba2
meoflaxz <meorhashim@gmail.com>
2025-11-03 23:38:36 +0800

    add base html

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
     """Display all posts"""
     posts = Post.objects.all()
+    # create variable all post in dictionary named context
     context = {'post': posts}
     return render(request, 'blog/post_list.html', context)
+
+def post_detail(request, pk):
+    """Display single post"""
+    post = get_object_or_404(Post, pk=pk)
+    context = {'post': post}
+    return render(request, 'blog/post_details.html', context)
 
 
